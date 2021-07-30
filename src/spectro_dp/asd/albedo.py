55d362c5abd07f3e9278826236094bf2f4537561
Joachim Meyer <j.meyer@utah.edu>
2021-07-30 09:49:37 -0600

    Albedo cli - rename script parameters.
    Increase clarity for the input parameters of the script and use more
    descriptive names. This also fixes a bug, where the sets were passed in
    the wrong order to the composite.

--- src/spectro_dp/asd/albedo.py
+++ src/spectro_dp/asd/albedo.py
@@ -8,28 +8,28 @@ from .plotter import Plotter
 @click.option('-in', '--input-dir', prompt=True, type=click.Path(exists=True))
 @click.option('-fp', '--file-prefix', prompt=True)
 @click.option(
-    '--up-looking-file-start', '-up', 'set_1_index', prompt=True, type=int
+    '--up-looking-file-start', '-up', 'up_index', prompt=True, type=int
 )
 @click.option(
-    '--up-looking-count', '-ulc', 'set_1_count', default=10, type=int
+    '--up-looking-count', '-ulc', 'up_count', default=10, type=int
 )
 @click.option(
-    '--down-looking-file-start', '-down', 'set_2_index', prompt=True, type=int
+    '--down-looking-file-start', '-down', 'down_index', prompt=True, type=int
 )
 @click.option(
-    '--down-looking-count', '-dlc', 'set_2_count', default=10, type=int
+    '--down-looking-count', '-dlc', 'down_count', default=10, type=int
 )
 @click.option('--debug', is_flag=True, default=False)
 def cli(
         input_dir, file_prefix,
-        set_1_index, set_1_count,
-        set_2_index, set_2_count,
+        up_index, up_count,
+        down_index, down_count,
         debug
 ):
     try:
         composite = MeasurementComposite(
-            input_dir, file_prefix, set_1_index, set_2_index,
-            set_1_count=set_1_count, set_2_count=set_2_count,
+            input_dir, file_prefix, down_index, up_index,
+            set_1_count=down_count, set_2_count=up_count,
             debug=debug
         )
         composite.calculate()
