98a5f1a1f3a3b18c1df5d01aeaa0f8ad3a0158fc
Joachim Meyer <j.meyer@utah.edu>
2021-11-09 16:32:22 -0700

    DISORT - compile - remove old binary
    Add step to remove the old binary before building a new one

--- DISORT/compile.sh
+++ DISORT/compile.sh
@@ -1,9 +1,16 @@
 #!/usr/bin/env bash
 
+EXECUTABLE="asd_albedo"
+
+# Remove old binary
+if [[ -f "${EXECUTABLE}" ]]; then
+  rm ${EXECUTABLE}
+fi
+
 # compile flies
 gfortran -O3 -g -fcheck=all -fdump-core -fbounds-check -Wall \
   getopt.F90  disort_variables.f90 asd_albedo.f90 code.f \
-  -o asd_albedo
+  -o ${EXECUTABLE}
 
 # Make exectuable
 chmod +x asd_albedo
