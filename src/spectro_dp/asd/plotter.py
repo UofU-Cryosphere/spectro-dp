44c1f8a5f2f00dfe8f579ce31d29a73274a6d360
Joachim Meyer <j.meyer@utah.edu>
2021-11-19 08:58:42 -0700

    ASD Plotter - Add basic method to plot single measurement.

--- src/spectro_dp/asd/plotter.py
+++ src/spectro_dp/asd/plotter.py
@@ -1,3 +1,5 @@
+from contextlib import contextmanager
+
 import matplotlib.pyplot as plt
 
 from .measurement_file import MeasurementFile
@@ -8,10 +10,36 @@ class Plotter:
     Base class to plot results of measurement composites
     """
 
-    AX1_TITLE = "Measurements"
+    TITLE = "Measurements"
     AX2_TITLE = "Ratio Set 1/Set 2"
 
+    FIGURE_DEFAULTS = dict(dpi=300)
     LINE_OPTS = dict(lw=1)
+
+    @staticmethod
+    @contextmanager
+    def show(**kwargs) -> None:
+        """
+        Wrapper to show a raw measurement.
+
+        :param kwargs: Arguments to pass to the plot
+            Options:
+             * 'composite_title': Title for the plot
+        """
+        plt.figure(**Plotter.FIGURE_DEFAULTS)
+
+        yield plt
+
+        plt.xlabel(MeasurementFile.X_LABEL)
+        plt.xlim(left=MeasurementFile.MIN_WAVELENGTH)
+        plt.ylabel(MeasurementFile.Y_LABEL)
+        plt.ylim(bottom=0)
+
+        plt.title(
+            kwargs.get('title', Plotter.TITLE)
+        )
+
+        plt.show()
 
     @staticmethod
     def show_composite(measurement_composite, **kwargs) -> None:
@@ -27,7 +55,7 @@ class Plotter:
              * 'set_2_label': Legend label for set_2
         """
         fig, (ax1, ax2) = plt.subplots(
-            2, 1, sharex=True, dpi=300
+            2, 1, sharex=True, **Plotter.FIGURE_DEFAULTS
         )
 
         label_set_1 = kwargs.get('set_1_label', 'Set 1')
@@ -35,7 +63,7 @@ class Plotter:
 
         x_ticks = MeasurementFile.BAND_RANGE
 
-        ax1.set_title(Plotter.AX1_TITLE)
+        ax1.set_title(Plotter.TITLE)
         ax1.plot(
             x_ticks, measurement_composite.set_1,
             label=label_set_1, c='goldenrod', **Plotter.LINE_OPTS
